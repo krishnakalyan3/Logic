fdaa7612c540e921512f7be72795e160fc977c87
krishna <krishnakalyan3@gmail.com>
2015-12-01 02:25:02 +0100

    Commit Date Tue Dec  1 02:25:01 CET 2015

--- PracticeSheets/practicesheet3.pl
+++ PracticeSheets/practicesheet3.pl
@@ -2,11 +2,28 @@
 %	'/Users/krishna/Dropbox/DMKM/Course/Logic and Knowledge Representation/Prolog_Code/PracticeSheets').
 
 % Factorial Without Tail Recursion
+
 % Basecase
-fac(0,1).
-fac(N,F):-
-	N>0,
-	N1 is N-1,
-	fac(N1,F1),
-	F is N*F1.
-	
+fact1(0,Result) :-
+    Result is 1.
+fact1(N,Result) :-
+    N > 0,
+    N1 is N-1,
+    fact1(N1,Result1),
+    Result is Result1*N.
+
+fact2(0,1).
+fact2(X,Y):-
+	X > 0,
+	X1 is X -1,
+	fact2(X1,Y1),
+	Y is Y1 * X.
+
+% 1st statement is for encapulation
+% 2nd predaictae cuts the tree and starts backtracking once the predicate conditions are staisfied
+fact3(N, R) :- fact3(N, 1, R).
+fact3(0, R, R) :- !.
+fact3(N, Acc, R) :-
+    NewN is N - 1,
+    NewAcc is Acc * N,
+    fact3(NewN, NewAcc, R).
